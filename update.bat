be042c908aa55c38628bd105d4d578d8f3b31627
LeMasurier, David <David.LeMasurier@Kronos.com>
2016-02-05 14:36:50 -0500

    adding scripts to help sync personal forks

---
+++ update.bat
@@ -0,0 +1,3 @@
+@echo off
+git remote add upstream https://github.com/perSEVERE-5962/driver_training-2016.git
+git pull upstream master
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git remote add upstream https://github.com/perSEVERE-5962/driver_training-2016.git
+git pull upstream master
